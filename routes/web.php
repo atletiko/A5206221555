309bc0d96f4a1f32d88c76d98d9bf616a077e6e6
atletiko <atletiko@gmail.com>
2023-11-24 10:19:26 +0700

    Pertemuan tgl 24 Nop 2023

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,11 @@ Route::post('/formulir/proses', 'App\Http\Controllers\DosenController@proses');
 Route::get('/blog', 'App\Http\Controllers\DosenController@home');
 Route::get('/blog/tentang', 'App\Http\Controllers\DosenController@tentang');
 Route::get('/blog/kontak', 'App\Http\Controllers\DosenController@kontak');
+
+//route CRUD
+Route::get('/pegawai','App\Http\Controllers\PegawaiController@index');
+Route::get('/pegawai/tambah','App\Http\Controllers\PegawaiController@tambah');
+Route::post('/pegawai/store','App\Http\Controllers\PegawaiController@store');
+Route::get('/pegawai/edit/{id}','App\Http\Controllers\PegawaiController@edit');
+Route::post('/pegawai/update','App\Http\Controllers\PegawaiController@update');
+Route::get('/pegawai/hapus/{id}','App\Http\Controllers\PegawaiController@hapus');
